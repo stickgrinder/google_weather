7e8443015235dd3fd2cb635d9c74d2500aa25741
Paolo Pustorino <paolo@heterogenius.it>
2011-02-22 14:05:02 +0100

    Template stubbed too, still needs revision!

--- google-weather-block.tpl.php
+++ google-weather-block.tpl.php
@@ -5,7 +5,6 @@
 ?>
 <div class="google-weather-block">
 
-    <?php $weather = new stdClass(); ?>
 
     <div class="leftbit">
         <div class="icon">
@@ -19,19 +18,40 @@
 
     <div class="content">
         <div class="location">
-        <form action="<?php //echo str_replace("&", "&amp;", $url); ?>" method="post">
-        <input type="text" name="weather_location" value="<?php // echo $weather_location; ?>" />
+        <form action="<?php echo str_replace("&", "&amp;", $url); ?>" method="post">
+        <input type="text" name="weather_location" value="<?php echo $weather_location; ?>" />
         </form>
         </div>
-    </div>
     <div class="rokweather-wrapper">
           <?php if (isset($weather->error)) :?>
           <div class="row error"><?php echo $weather->error; ?></div>
           <?php else: ?>
-          <div class="row"><?php //echo $weather->current_condition; ?></div>
-          <div class="row"><?php //echo $weather->current_humidity; ?></div>
-          <div class="row"><?php //echo $weather->current_wind; ?></div>
-          <div class="forecast"></div>
-          <?php endif; ?>
+          <div class="row"><?php echo $weather->current_condition; ?></div>
+          <div class="row"><?php echo $weather->current_humidity; ?></div>
+          <div class="row"><?php echo $weather->current_wind; ?></div>
+          <div class="forecast">
+
+      <?php
+        // $weather->forecast = array_slice($weather->forecast, 0, $params->get('forcast_show', 4));
+      ?>
+
+          <?php foreach ($weather->forecast as $day): ?>
+              <div class="day">
+                  <span><?php echo $day->day_of_week; ?></span><br />
+                  <img src="<?php echo $icon_url.$day->icon; ?>" alt="" /><br />
+                  <div class="degf">
+                      <span class="low"><?php echo $day->low_f; ?></span> |
+                      <span class="high"><?php echo $day->high_f; ?></span>
+                  </div>
+                  <div class="degc">
+                      <span class="low"><?php echo $day->low_c; ?></span> |
+                      <span class="high"><?php echo $day->high_c; ?></span>
+                  </div>
+              </div>
+              <?php endforeach; ?>
+          </div>
+        </div>
+        <?php endif; ?>
     </div>
+
 </div>
