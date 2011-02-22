82eb9c345659595b2ca59d5ee863da0b1e677a20
Paolo Pustorino <paolo@heterogenius.it>
2011-02-22 19:27:13 +0100

    HTML template finished. Selectors are there to be used in JS.

--- google-weather-block.tpl.php
+++ google-weather-block.tpl.php
@@ -5,52 +5,53 @@
 ?>
 <div class="google-weather-block">
 
-    <div class="leftbit">
+    <div class="location">
+      <form action="<?php print str_replace("&", "&amp;", $url_ajax); ?>" method="post">
+        <input type="text" name="weather_location" value="<?php print $weather_location; ?>" />
+      </form>
+      <h2><?php print $weather_location; ?></h2>
+    </div> <!-- location information and form -->
+
+    <?php if (isset($weather->error)) :?>
+
+    <div class="error">
+      <?php print $weather->error; ?>
+    </div>
+
+    <?php else: // there is no error and info are available ?>
+    <div class="today">
         <div class="icon">
-            <img src="<?php echo $icon_url.$weather->icon; ?>" alt="" />
-            <div class="degf"><?php echo $weather->current_temp_f;?>&deg;</div>
-            <div class="degc"><?php echo $weather->current_temp_c;?>&deg;</div>
+            <img src="<?php print $icon_url.$weather->icon; ?>" alt="" />
+            <div class="tempf"><?php print $weather->current_temp_f;?>&deg;</div>
+            <div class="tempc"><?php print $weather->current_temp_c;?>&deg;</div>
         </div>
-        <div class="degrees"><span class="active">&deg;F</span> | <span>&deg;C</span></div>
+        <div class="degree_types">
+          <a href="#" title="<?php echo t("View temperatures in Celsius"); ?>" <?php if ($degree_type == 'C'): ?>class="active"<?php endif; ?>>&deg;C</a> |
+          <a href="#" title="<?php echo t("View temperatures in Farenheit"); ?>" <?php if ($degree_type == 'F'): ?>class="active"<?php endif; ?>>&deg;F</a>
+        </div>
+        <div class="info">
+          <div class="row"><?php print $weather->current_condition; ?></div>
+          <div class="row"><?php print $weather->current_humidity; ?></div>
+          <div class="row"><?php print $weather->current_wind; ?></div>
+        </div>
     </div>
 
-
-    <div class="content">
-        <div class="location">
-        <form action="<?php echo str_replace("&", "&amp;", $url); ?>" method="post">
-        <input type="text" name="weather_location" value="<?php echo $weather_location; ?>" />
-        </form>
-        </div>
-    <div class="rokweather-wrapper">
-          <?php if (isset($weather->error)) :?>
-          <div class="row error"><?php echo $weather->error; ?></div>
-          <?php else: ?>
-          <div class="row"><?php echo $weather->current_condition; ?></div>
-          <div class="row"><?php echo $weather->current_humidity; ?></div>
-          <div class="row"><?php echo $weather->current_wind; ?></div>
-          <div class="forecast">
-
-      <?php
-        // $weather->forecast = array_slice($weather->forecast, 0, $params->get('forcast_show', 4));
-      ?>
-
-          <?php foreach ($weather->forecast as $day): ?>
-              <div class="day">
-                  <span><?php echo $day->day_of_week; ?></span><br />
-                  <img src="<?php echo $icon_url.$day->icon; ?>" alt="" /><br />
-                  <div class="degf">
-                      <span class="low"><?php echo $day->low_f; ?></span> |
-                      <span class="high"><?php echo $day->high_f; ?></span>
-                  </div>
-                  <div class="degc">
-                      <span class="low"><?php echo $day->low_c; ?></span> |
-                      <span class="high"><?php echo $day->high_c; ?></span>
-                  </div>
-              </div>
-              <?php endforeach; ?>
+    <div class="forecasts">
+      <?php foreach ($weather->forecast as $day): ?>
+      <div class="day">
+          <h3><?php print $day->day_of_week; ?></h3>
+          <img src="<?php print $icon_url.$day->icon; ?>" alt="" />
+          <div class="tempf">
+              <span class="low"><?php print $day->low_f; ?></span> |
+              <span class="high"><?php print $day->high_f; ?></span>
           </div>
-        </div>
-        <?php endif; ?>
-    </div>
+          <div class="tempc">
+              <span class="low"><?php print $day->low_c; ?></span> |
+              <span class="high"><?php print $day->high_c; ?></span>
+          </div>
+      </div>
+      <?php endforeach; ?>
+    </div> <!-- .forecasts -->
+    <?php endif; // if there has been an error ?>
 
 </div>
