cf938586a19c19d16d93df914a4b082f38a79d5f
Paolo Pustorino <paolo@heterogenius.it>
2011-02-24 12:41:47 +0100

    Added icon for search field, some hacking attempt in JS code

--- google_weather.js
+++ google_weather.js
@@ -2,52 +2,46 @@
 
 if (Drupal.jsEnabled) {
   $(document).ready(function () {
-		inizializza();
+    google_weather_init();
   });
-	Drupal.behaviors.myBehaviorsFunction = myBehaviorsFunction;
+  Drupal.behaviors.google_weather.attach_events = google_weather_attachevents;
 }
 
-function myBehaviorsFunction() {
-  inizializza();
+function google_weather_attachevents() {
+  google_weather_init();
 }
 
-function inizializza(){
+function google_weather_init(){
     $('#google_weather_location_i').hide();
     $('#google_weather_location_l').show();
 
-		$('#weather_form').submit(function() {
-			var google_weather_location_value=$('#google_weather_location_i').val();
-			$.ajax({
-				type: 'POST',
-  			url: "google_weather_block_ajax",
-				cache: false,
-				data: "google_weather_location=" + $('#google_weather_location_i').val(),
-  			//context: document.body,
-  			success: function(data){
-    			$('#google_weather_location_i').hide();
-    			$('#google_weather_location_l').show();
-					$(".google-weather-block").parent().html(data);
-    			$('#google_weather_location_l').html(google_weather_location_value);
-    			$('#google_weather_location_i').val(google_weather_location_value);
-    			$('#google_weather_location_i').hide();
-    			$('#google_weather_location_l').show();
-					$('#google_weather_location_l').click(function() {
+    $('#weather_form').submit(function() {
+      var google_weather_location_value=$('#google_weather_location_i').val();
+      $.ajax({
+        type: 'POST',
+        url: "google_weather_block_ajax",
+        cache: false,
+        data: "google_weather_location=" + $('#google_weather_location_i').val(),
+        //context: document.body,
+        success: function(data){
+          $('#google_weather_location_i').hide();
+          $('#google_weather_location_l').show();
+          $(".google-weather-block").parent().html(data);
+          Drupal.attachBehaviors($(".google-weather-block").parent());
+          $('#google_weather_location_l').html(google_weather_location_value);
+          $('#google_weather_location_i').val(google_weather_location_value);
+          $('#google_weather_location_i').hide();
+          $('#google_weather_location_l').show();
+        }
+      });
+      return false;
+    });
 
-    				$('#google_weather_location_i').show();
-    				$('#google_weather_location_l').hide();
-    				$('#google_weather_location_i').focus();
+    $('#google_weather_location_l').click(function() {
 
-					});
-  			}
-			});
-  		return false;
-		});
-		
-		$('#google_weather_location_l').click(function() {
+      $('#google_weather_location_i').show();
+      $('#google_weather_location_l').hide();
+      $('#google_weather_location_i').focus();
 
-    	$('#google_weather_location_i').show();
-    	$('#google_weather_location_l').hide();
-    	$('#google_weather_location_i').focus();
-
-		});
+    });
 }
